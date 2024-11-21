85ca0f2805987435d6d4ee6719fbbc9c165574c0
artem <artialbk@gmail.com>
2024-11-21 20:56:56 +0300

    21.11.24

--- src/App.js
+++ src/App.js
@@ -4,26 +4,66 @@ import Counter from './components/Counter/Counter'
 import Controllers from './components/Controllers/Controllers';
 
 import RobotList from './components/RobotList/RobotList';
-import Robot from './components/Robot/Robot';
+import SearchField from './components/SearchField/SearchFied';
 
-import {useState} from 'react'
-import {users} from './users'
+import {useState, useEffect} from 'react'
+import {URL} from './constants'
 
 function App() {
   const [count, setCount] = useState(1)
   const plus = () => setCount(count + 1)
   const minus = () => setCount(count - 1)
 
-  const [robots, setRobots] = useState(users)
+  const [robots, setRobots] = useState([])
+  const [error,setError] = useState(null)
+  const[loading,setLoading]= useState(true)
+  const [searchField, setSearchField] = useState('')
+
+  const searchHandler = (e) => {
+    setSearchField(e.target.value)
+  }
+  
+
+  useEffect(() => {
+    async function fetchRobots(){
+      let response = await fetch(URL)
+      let robots = await response.json()
+      setRobots(robots)
+    }
+
+    
+    fetchRobots()
+      .catch(error => setError(error))
+      .finally(() => {
+        setLoading(false)
+      })
+  }, [URL])
+
+  const filteredRobots = robots.filter(robot => {
+    return  robot.name.toLowerCase().includes(searchField.toLocaleLowerCase())||
+            robot.username.toLowerCase().includes(searchField.toLocaleLowerCase()) || 
+            robot.email.toLowerCase().includes(searchField.toLocaleLowerCase())
+          })
+
   return (
+    loading ? <h1>загорузка</h1> :
+    error ? <h1>ошитбка</h1> :
     <div className="App">
       <header className="App-header">
-        <RobotList robots = {robots}/>
+        <SearchField onChange = {searchHandler}/>
+      </header>
+      <main className="App-main">
+        {
+          loading ? <h1>загорузка</h1> :
+          error ? <h1>ошитбка</h1> :
+        <RobotList robots = {filteredRobots}/>
+        }
         <div className='counts'>
           <Counter count={ count } />
           <Controllers plus={plus} minus={minus}/>
         </div>
-      </header>
+
+      </main>
     </div>
   );
 }
---
+++ src/components/SearchField/SearchFied.jsx
@@ -0,0 +1,7 @@
+import './SearchField.css'
+
+export default function SearchField({onChange}){
+    return(
+        <input className="searchfield" type="text" placeholder='Search' onChange={onChange}/>
+    )
+}
